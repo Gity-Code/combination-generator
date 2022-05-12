e849cb9eb7e8f656b109adc19cbff678e54ab4df
Gity-Code <gity1010@gmail.com>
2022-05-12 11:28:13 +0300

    finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CombinationModule } from './combination/combination.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+//   loadChildren: () =>
+//   import(
+//     './combination/combination.module'
+// ).then((m) => m.CombinationModule)
+  
+{
+  path: '',
+  loadChildren: () => CombinationModule,
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from 'src/matrial/matrial.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+     MaterialModule,
+     FormsModule, 
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
